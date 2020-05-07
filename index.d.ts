eb6ca753f24568f579c7414399822bc68abe86dc
Hien Phung <pkhien95@gmail.com>
2020-05-07 14:00:52 +0700

    Add ts definition

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,29 @@
+declare module 'react-native-apple-music' {
+  export enum AuthorizationStatus {
+    authorizationStatusNotDetermined,
+    authorizationStatusDenied,
+    authorizationStatusRestricted,
+    authorizationStatusAuthorized
+  }
 
+  export enum Capability {
+    capabilityNone,
+    capabilityMusicCatalogPlayback,
+    capabilityMusicCatalogSubscriptionEligible,
+    capabilityAddToCloudMusicLibrary
+  }
+
+  export interface Auth {
+    requestAuthorization: () => Promise<AuthorizationStatus|any>
+    getStoreFrontCountryCode: () => Promise<string|any>
+    requestUserToken: (developerToken: string) => Promise<string|any>
+    requestCapabilities: () => Promise<Capability|any>
+  }
+
+  export interface MediaPlayer {
+    setQueue: (storeIDs: string[]) => void
+    play: () => void
+    pause: () => void
+    stop: () => void
+  }
+}
